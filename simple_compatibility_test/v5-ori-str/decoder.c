9c7e2dce2cbac831e72afc6b3ec125f6183cc02d
BearLin <bear.lin.001@gmail.com>
2017-04-18 00:33:44 +0800

    Add simple_compatibility_test/ and it's results in simple_compatibility_test/result.txt

---
+++ simple_compatibility_test/v5-ori-str/decoder.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <pb_encode.h>
+#include <pb_decode.h>
+#include "simple.pb.h"
+
+int main()
+{
+    /* This is the buffer where we will store our message. */
+    uint8_t buffer_decode[128];
+    size_t message_length;
+    bool status;
+
+    /* Decode from file. */
+    {
+        FILE *pFile = fopen("data.bin", "rb");
+        if (pFile == NULL)
+        {
+            printf("Error opening file, data.bin!\n");
+            return 1;
+        }
+        else
+        {
+            // Get file size
+            fseek(pFile, 0, SEEK_END);   // non-portable
+            message_length = ftell(pFile);
+            rewind(pFile);
+
+            // Read to buffer_decode
+            fread(buffer_decode, sizeof(char), message_length, pFile);
+            fclose(pFile);
+        }
+
+        // Ready to decode!
+        SimpleMessage message = SimpleMessage_init_zero;
+
+        pb_istream_t stream = pb_istream_from_buffer(buffer_decode, message_length);
+
+        status = pb_decode(&stream, SimpleMessage_fields, &message);
+        if (!status)
+        {
+            printf("Decoding failed: %s\n", PB_GET_ERROR(&stream));
+            return 1;
+        }
+
+        /* Print the data contained in the message. */
+        if (message.has_num1)
+            printf("num1:%d!\n", message.num1);
+        if (message.has_num2)
+            printf("num2:%d!\n", message.num2);
+        if (message.has_num3)
+            printf("num3:%d!\n", message.num3);
+        if (message.has_name1)
+            printf("name1:\"%s\"!\n", message.name1);
+
+        return 0;
+    }
+}
---
+++ simple_compatibility_test/v8-new-str-at-middle-re-numbering/encoder.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <pb_encode.h>
+#include <pb_decode.h>
+#include "simple.pb.h"
+
+int main()
+{
+    uint8_t buffer_encode[128];
+    size_t message_length;
+    bool status;
+
+    /* Encode our message */
+    {
+        SimpleMessage message = SimpleMessage_init_zero;
+        pb_ostream_t stream = pb_ostream_from_buffer(buffer_encode, sizeof(buffer_encode));
+
+        /* Fill */
+        message.has_num1 = true;
+        message.num1 = 14;
+        message.has_num2 = true;
+        message.num2 = 15;
+        message.has_num3 = true;
+        message.num3 = 16;
+        message.has_name1 = true;
+        strncpy(message.name1, "the_name1", strlen("the_name1"));
+        message.has_name2 = true;
+        strncpy(message.name2, "the_name2", strlen("the_name2"));
+
+        status = pb_encode(&stream, SimpleMessage_fields, &message);
+        message_length = stream.bytes_written;
+        if (!status)
+        {
+            printf("Encoding failed: %s\n", PB_GET_ERROR(&stream));
+            return 1;
+        }
+
+        // print buffer_encode[] content
+        printf("message_length:%ld\n", message_length);
+        for (int i = 0; i < message_length; i++)
+        {
+            if (i%4 == 3)
+                printf("0x%02x \n", buffer_encode[i]);
+            else
+                printf("0x%02x ", buffer_encode[i]);
+        }
+        printf("\n");
+    }
+
+    // Write to file
+    FILE *pFile = fopen("data.bin", "wb");
+    if (pFile == NULL)
+    {
+        printf("Error opening file!\n");
+        return 1;
+    }
+    else
+    {
+        fwrite(buffer_encode, sizeof(char), message_length, pFile);
+        fclose(pFile);
+    }
+
+    return 0;
+}
+
